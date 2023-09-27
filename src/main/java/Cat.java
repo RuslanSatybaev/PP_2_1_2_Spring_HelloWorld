81e1cc4fc9551af6e3cf3f648618b8de2acd7d4d
Ruslan Satybaev <medtehnik850@gmail.com>
2023-09-27 22:14:13 +0700

    HW-2-1-3: Add Cat class, add bean

---
+++ src/main/java/Cat.java
@@ -0,0 +1,23 @@
+public class Cat {
+    private String name;
+    private int legs;
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLegs() {
+        return legs;
+    }
+
+    public void setLegs(int legs) {
+        this.legs = legs;
+    }
+}
